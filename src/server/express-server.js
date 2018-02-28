37ef4fa6ff3bd286d131737010df728f966038d3
Mel hsieh <mehsieh89@gmail.com>
2018-02-28 04:51:31 -0800

    testing deployment with db, pathway issue

--- src/server/express-server.js
+++ src/server/express-server.js
@@ -56,17 +56,17 @@ const startServer = () =>
       }
     });
   });
-  
+
 const db = require('../db');
 
-app.get('/test', function(req, res) {
-  console.log('server url hit');
-  db.find({}, function(err, data) {
-   if (err) throw err;
-   res.send(data);
-   res.end();
- })
-})
+// app.get('/test', function(req, res) {
+//   console.log('server url hit');
+//   db.find({}, function(err, data) {
+//    if (err) throw err;
+//    res.send(data);
+//    res.end();
+//  })
+// })
 
 module.exports = function electrodeServer(userConfig, callback) {
   const promise = Promise.resolve(userConfig)
